08eb0cdc2bb529ff1ea74df3cd087c7f11693bd8
eksys <potential777@gmail.com>
2016-11-13 17:03:35 +0600

    Процедурный подход. Функции

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,25 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 1;
+            int y1 = 3;
+            char sum1 = '*';
+
+            Draw(x1, y1, sum1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sum2 = '#';
+
+            Draw(x2, y2, sum2);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sum)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sum);
         }
     }
 }
