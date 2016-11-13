06637f5622f2cbfd10107d915a2c82164d41a679
eksys <potential777@gmail.com>
2016-11-13 22:25:42 +0600

    Горизонтальная и вертикальная линия

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,38 +1,26 @@
-﻿using snake;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleSnake
+namespace snake
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
 
-            List<char> CharList = new List<char>();
-            CharList.Add('*');
-            CharList.Add('#');
-            CharList.Add('@');
-            CharList.Add('$');
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
 
-            Point p1 = new Point(1, 3, CharList[0]);
-            Point p2 = new Point(4, 5, CharList[1]);
-            Point p3 = new Point(6, 8, CharList[2]);
-            Point p4 = new Point(10, 13, CharList[3]);
+            HorizontalLine Line = new HorizontalLine(5, 10, 8, '+');
+            Line.Draw();
 
-            List<Point> PointList = new List<Point>();
-            PointList.Add(p1);
-            PointList.Add(p2);
-            PointList.Add(p3);
-            PointList.Add(p4);
-
-            foreach (Point i in PointList)
-            {
-                i.Draw();
-            }
+            VerticalLine Line1 = new VerticalLine(5, 10, 8, '+');
+            Line1.Draw();
 
             Console.ReadLine();
         }
