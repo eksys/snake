b3b539df9678a6e387f6d255a3b7e34e4399d3c3
eksys <potential777@gmail.com>
2016-11-14 20:33:11 +0600

    Изучаем полиморфизм

--- snake/snake/Figure.cs
+++ snake/snake/Figure.cs
@@ -9,7 +9,7 @@ namespace snake
     class Figure
     {
         protected List<Point> pList;
-        public void Draw()
+        public virtual void Draw()
         {
             foreach (Point p in pList)
             {
